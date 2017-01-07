238e98658a0449dc3c6d5c6f25708b31783263aa
Samiran Majumder <smajumder@sdl.com>
2017-01-07 16:56:01 +0530

    add router for back navigation

--- APM/app/products/product-detail.component.ts
+++ APM/app/products/product-detail.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import { ActivatedRoute , Router} from '@angular/router';
 import { Iproduct } from './product';
 
+
 @Component({
-    templateUrl:"app/products/product-detail.component.html";
+    templateUrl:"app/products/product-detail.component.html"
 })
-export class ProductDetailComponent{
+export class ProductDetailComponent implements OnInit{
     pageTitle:string="Product Detail";
     product:Iproduct; 
+
+    constructor(private route:ActivatedRoute, private router:Router ){
+        console.log( this.route.snapshot.params["id"]);
+    }
+
+    ngOnInit (): void{
+        let id=this.route.snapshot.params["id"];
+        this.pageTitle+=` : ${id}`;
+    }
+
+    onBack():void{
+        this.router.navigate(['/products']);
+    }
+
 }
