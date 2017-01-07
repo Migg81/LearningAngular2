c8c6fcdaa61c824d28d55602817aeae7c562bf6b
Samiran Majumder <smajumder@sdl.com>
2017-01-07 15:37:19 +0530

    Add minor fix in app component

--- APM/app/app.component.ts
+++ APM/app/app.component.ts
@@ -8,7 +8,7 @@ import { RouterModule, Routes } from '@angular/router';
 <div>
         <nav class='navbar navbar-default'>
             <div class='container-fluid'>
-                <a class="navbar-brand'>{{pageTitle}}</a>
+                <a class='navbar-brand'>{{pageTitle}}</a>
                 <ul class='nav navbar-nav'>
                     <li><a [routerLink]="['/welcome']">Home</a></li>
                     <li><a [routerLink]="['/products']">Product List</a></li>
