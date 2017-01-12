c08ca86455b3d318e4e007e857ed2b28f358c9c5
Samiran Majumder <smajumder@sdl.com>
2017-01-12 17:53:38 +0530

    Added app routing module

--- APM/app/app.module.ts
+++ APM/app/app.module.ts
@@ -2,23 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from "@angular/forms";
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { ProductListComponent } from './products/product-list.component';;
 import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { ProductModule } from './products/product.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule ,
-    HttpModule, 
-    RouterModule.forRoot([
-      {path:"welcome", component: WelcomeComponent},
-      {path:"**", redirectTo: "welcome",pathMatch:"full"}
-    ]),
-    ProductModule
+    HttpModule,     
+    ProductModule,
+    AppRoutingModule
   ],
   declarations: [ 
     AppComponent ,
